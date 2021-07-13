7b96b2e43b227355cc401136ef627ac8c5cd4f6a
YaroslavPershakov <87316522+YaroslavPershakov@users.noreply.github.com>
2021-07-13 22:28:59 +0300

    Add files via upload

---
+++ src/MyUtil/Main.java
@@ -0,0 +1,9 @@
+package MyUtil;
+
+public class Main {
+
+    public void error(String className, String message) {
+        System.out.println("В классе " + className + " ошибка: " + message);
+    }
+
+}
